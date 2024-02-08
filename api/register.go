3c3a3f87e785d326b6bf000502a8efde98a29d66
Lucas TESSON <lucastesson@protonmail.com>
2024-02-08 15:36:06 +0100

    fix(register): fix nonce & session extract

--- api/register.go
+++ api/register.go
@@ -12,7 +12,6 @@ type RegisterParams struct {
 	Name     string
 	Email    string
 	Password string
-	Nonce    string // XXX this should not be part of the API
 }
 
 func (client *Client) Register(params *RegisterParams, opts ...Option) error {
@@ -20,7 +19,7 @@ func (client *Client) Register(params *RegisterParams, opts ...Option) error {
 	val.Set("name", params.Name)
 	val.Set("email", params.Email)
 	val.Set("password", params.Password)
-	val.Set("nonce", params.Nonce)
+	val.Set("nonce", client.nonce) // XXX this should not be part of the API
 	val.Set("_submit", "Submit")
 
 	req, _ := http.NewRequest(http.MethodPost, "/register", bytes.NewBufferString(val.Encode()))
@@ -32,21 +31,33 @@ func (client *Client) Register(params *RegisterParams, opts ...Option) error {
 	defer res.Body.Close()
 
 	if res.StatusCode != http.StatusFound {
-		return fmt.Errorf("CTFd responded with status code %d", res.StatusCode)
+		return fmt.Errorf("CTFd responded with status code %d, which could be due to email reuse", res.StatusCode)
 	}
 
-	// Update nonce and session
+	// Update session to track user then fetch nonce for later API calls
+	cookieFound := false
+	for _, cookie := range res.Cookies() {
+		if cookie.Name == "session" {
+			client.session = cookie.Value
+			cookieFound = true
+			break
+		}
+	}
+	if !cookieFound {
+		return errors.New("session cookie not found, may be due to server misconfiguration (not setup yet) or API instability")
+	}
+	req, _ = http.NewRequest(http.MethodGet, "/", nil)
+	res, err = client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
 	nonce, err := getNonce(res.Body)
 	if err != nil {
 		return err
 	}
 	client.nonce = nonce
 
-	for _, cookie := range res.Cookies() {
-		if cookie.Name == "session" {
-			client.session = cookie.Value
-			return nil
-		}
-	}
-	return errors.New("session cookie not found")
+	return nil
 }
