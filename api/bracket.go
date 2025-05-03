128a1be2f7723df499e62409cf579ea088d918a0
Lucas Tesson <lucastesson@protonmail.com>
2025-05-03 22:59:42 +0200

    feat(api): improve handling of brackets (#153)

--- api/bracket.go
+++ api/bracket.go
@@ -1,6 +1,13 @@
 package api
 
-type GetBracketsParams struct{}
+import "fmt"
+
+type GetBracketsParams struct {
+	Name        *string `schema:"name,omitempty"`
+	Description *string `schema:"description,omitempty"`
+	Type        *string `schema:"type,omitempty"`
+	Q           *string `schema:"q,omitempty"`
+}
 
 func (client *Client) GetBrackets(params *GetBracketsParams, opts ...Option) ([]*Bracket, error) {
 	bks := []*Bracket{}
@@ -20,6 +27,20 @@ type PostBracketsParams struct {
 func (client *Client) PostBrackets(params *PostBracketsParams, opts ...Option) (*Bracket, error) {
 	bk := &Bracket{}
 	if err := client.Post("/brackets", params, bk, opts...); err != nil {
+		return nil, err
+	}
+	return bk, nil
+}
+
+type PatchBracketsParams struct {
+	Name        *string `json:"name,omitempty"`
+	Description *string `json:"description,omitempty"`
+	Type        *string `json:"type,omitempty"`
+}
+
+func (client *Client) PatchBrackets(id int, params *PatchBracketsParams, opts ...Option) (*Bracket, error) {
+	bk := &Bracket{}
+	if err := client.Patch(fmt.Sprintf("/brackets/%d", id), params, bk, opts...); err != nil {
 		return nil, err
 	}
 	return bk, nil
